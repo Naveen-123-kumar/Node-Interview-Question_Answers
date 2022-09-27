d41e7562a4013887ea926b652e8c8939e66ece25
NAVEEN KUMAR <1998naveenk@gmail.com>
2022-09-27 17:02:14 +0530

    Node day1 has been completed

---
+++ Day1/Day1.js
@@ -0,0 +1,20 @@
+// 1) What is Nodejs?
+// Ans->Node is free open-source cross-platform backend javascript run time environment that executes on crome's v8 engine and execute javascript code outside web browser.
+//nodejs operates on single thread,non-blocking io codes.
+//it is used for easily building fast and scalable network applications
+
+//2) What is NPM?
+// Ans->NPM stands for Node Package Manager.It is world's largest software registry.We use NPM to install packages that will be helpfull in building application.The packages install with NPM is installed globally.
+
+// 3) What are different modules in Nodejs?
+// Ans->Modules are block of code or functions that is used in our application to communicate with external files.It is used because of reuseablility and code can be broken into small pieces.There are three types of mudules i)Core modules ii)Local modules iii) Third party modules.Ex-fs,http
+
+
+// 4) What is the purpose of the module.exports?
+// Ans->The purpose of module.exports is that we can use it in another file.It promotes the modular programming,it means we can create small chunk of code and reuse it again and again so where ever it will be required and our code will look clean and easily manageable.
+
+// 5)Difference between default export and named export?
+//Ans->The difference between default export and named export is that by using defalt export we can export only one function,data or object but by named export we can exports multiple names.It should be in curly braces.
+
+// 6)How do you import any module in Node.js?
+// Ans->We import a module in node js by using keyword require('module-name').
